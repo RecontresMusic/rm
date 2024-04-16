3ebcae8819653adbdc11e162ba4e8e4149b74d9d
Danila SIdorkin <sidorfaceit@gmail.com>
2024-04-16 04:59:35 +0300

    1

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -1,146 +1,24 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
 
 
 namespace rm
 {
-    /// <summary>
-    /// Логика взаимодействия для Window1.xaml
-    /// </summary>
 
-    using System.ComponentModel;
-    using System.Windows.Input;
-    using System.Runtime.CompilerServices;
-    using System.IO;
-    using System.Windows.Media.Animation;
-    using System.Windows.Shapes;
-    using System.Windows.Media;
-    using System.Collections.ObjectModel;
-
+    #region ViewModel
     public class ViewModel : INotifyPropertyChanged
     {
-        private Visibility _elementsVisibility = Visibility.Visible;
-        private Brush _background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF656565"));
-        private Brush _foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFEDEDED"));
-        private Brush _borderbrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF656565")); 
-        private string _buttonContent = "↩️";
-        public ObservableCollection<string> Items { get; set; }
-
-        public ICommand AddItemCommand { get; private set; }
-        public ICommand RemoveItemCommand { get; private set; }
-        private string _selectedItem;
-
-        public string SelectedItem
-        {
-            get => _selectedItem;
-            set
-            {
-                _selectedItem = value;
-                OnPropertyChanged(nameof(SelectedItem));
-            }
-        }
-
-        private void AddItem()
-        {
-            // Добавление нового элемента в коллекцию
-            Items.Add("Playlist #" + (Items.Count + 1));
-        }
-
-        public Visibility ElementsVisibility
-        {
-            get => _elementsVisibility;
-            set
-            {
-                if (_elementsVisibility != value)
-                {
-                    _elementsVisibility = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public Brush Background
-        {
-            get => _background;
-            set
-            {
-                if (_background != value)
-                {
-                    _background = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public Brush Foreground
-        {
-            get => _foreground;
-            set
-            {
-                if (_foreground != value)
-                {
-                    _foreground = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public Brush BorderBrush
-        {
-            get => _borderbrush;
-            set
-            {
-                if (_borderbrush != value)
-                {
-                    _borderbrush = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public string ButtonContent
-        {
-            get => _buttonContent;
-            set
-            {
-                if (_buttonContent != value)
-                {
-                    _buttonContent = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-
-
-        public ICommand ToggleVisibilityCommand { get; private set; }
-
-        private void ToggleVisibility()
-        {
-            ElementsVisibility = ElementsVisibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible; 
-
-            var currentColor = ((SolidColorBrush)_background).Color.ToString();
-            var newColorHex = currentColor == "#FF656565" ? "#FFEDEDED" : "#FF656565";
-            Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(newColorHex));
-
-            var currentColor2 = ((SolidColorBrush)_foreground).Color.ToString();
-            var newColorHex2 = currentColor2 == "#FFEDEDED" ? "#FF656565" : "#FFEDEDED";
-            Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString(newColorHex2));
-
-            var currentColor3 = ((SolidColorBrush)_borderbrush).Color.ToString();
-            var newColorHex3 = currentColor3 == "#FF656565" ? "#FFEDEDED" : "#FF656565";
-            BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString(newColorHex3));
-
-            ButtonContent = ButtonContent == "↩️" ? "↪️" : "↩️";
-        }
-
         private bool _isPlaying;
-        private Thickness _iconMargin = new Thickness(20);
-        private double _iconWidth = 20;
-        private double _iconHeight = 30;
-        private string _playPauseIcon;
-
 
         public bool IsPlaying
         {
@@ -151,104 +29,95 @@ namespace rm
                 {
                     _isPlaying = value;
                     OnPropertyChanged();
-                    OnPropertyChanged(nameof(IconData)); 
-                    IconMargin = IsPlaying ? new Thickness(18, 0, 0, 0) : new Thickness(20, 0, 0, 0); // Изменение отступов
-                    OnPropertyChanged(nameof(IconMargin));
-                    IconWidth = IsPlaying ? 20 : 20;
-                    IconHeight = IsPlaying ? 20 : 25;
-                    OnPropertyChanged(nameof(IconMargin));
-                    OnPropertyChanged(nameof(IconWidth));
-                    OnPropertyChanged(nameof(IconHeight));
                 }
             }
         }
+        private Dictionary<string, Track> _tracksDictionary;
+        private const string TracksFilePath = "tracks.json"; // Путь к файлу для сохранения треков
 
-        public string IconData => IsPlaying ? "m 10 13 L 10 4 H 3 L 3 13 Z V 4 L 14 4 H 14 H 14 V 13 Z" : "M0,0 L0,40 30,20 Z";
-        public Thickness IconMargin
+        public Dictionary<string, Track> TracksDictionary
         {
-            get => _iconMargin;
+            get => _tracksDictionary;
             set
             {
-                if (_iconMargin != value)
-                {
-                    _iconMargin = value;
-                    OnPropertyChanged(nameof(IconMargin));
-                }
+                _tracksDictionary = value;
+                OnPropertyChanged();
             }
         }
 
-        public double IconWidth
-        {
-            get => _iconWidth;
-            set
-            {
-                if (_iconWidth != value)
-                {
-                    _iconWidth = value;
-                    OnPropertyChanged(nameof(IconWidth));
-                }
-            }
-        }
 
-        public double IconHeight
-        {
-            get => _iconHeight;
-            set
-            {
-                if (_iconHeight != value)
-                {
-                    _iconHeight = value;
-                    OnPropertyChanged(nameof(IconHeight));
-                }
-            }
-        }
-
-        public ICommand TogglePlayCommand { get; }
-        //System.Windows.Data Error: 40 : BindingExpression path error: 'RemoveItemCommand' property not found on 'object' ''String' (HashCode=-830691222)'. BindingExpression:Path=RemoveItemCommand; DataItem='String' (HashCode=-830691222); target element is 'MenuItem' (Name=''); target property is 'Command' (type 'ICommand')
+        #region WorkWithTracks
+        public ICommand OpenFileCommand { get; private set; }
         public ViewModel()
         {
-            TogglePlayCommand = new RelayCommand(ExecuteTogglePlayCommand);
-            _iconMargin = new Thickness(20, 0, 0, 0);
-            _iconWidth = 20;
-            _iconHeight = 25;
-            _playPauseIcon = "M 0 0 L 15 0 L 15 30 L 0 30 Z M 15 0 L 30 0 L 30 30 L 15 30 Z";
-            ToggleVisibilityCommand = new RelayCommand(ToggleVisibility);
-            // Инициализация коллекции элементов
-            Items = new ObservableCollection<string>{
-                "I",
-            "i",
-            "i",
-            "i",
-            "i",
-                "I",
-            "i",
-            "i",
-            "i",
-            "i",
-                "I",
-            "i",
-            "i",
-            "i",
-            "i",
-            };
-            AddItemCommand = new RelayCommand(AddItem);
-            RemoveItemCommand = new RelayCommand(RemoveItem, () => SelectedItem != null);
-            Console.WriteLine("1");
+            LoadTracks();
+            OpenFileCommand = new RelayCommand(OpenFileDialog);
         }
-
-        private void RemoveItem()
+        private void LoadTracks()
         {
-            Console.WriteLine("1");
-            if (SelectedItem != null)
+            if (File.Exists(TracksFilePath))
             {
-                Items.Remove(SelectedItem);
+                try
+                {
+                    string json = File.ReadAllText(TracksFilePath);
+                    _tracksDictionary = JsonSerializer.Deserialize<Dictionary<string, Track>>(json) ?? new Dictionary<string, Track>();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Error loading tracks: " + ex.Message);
+                    // Здесь можете добавить более подробную обработку ошибок
+                }
+            }
+            else
+            {
+                _tracksDictionary = new Dictionary<string, Track>();
+            }
+        }
+        private void OpenFileDialog()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".mp3"; // Расширение файлов по умолчанию
+            dlg.Filter = "Audio files (*.mp3)|*.mp3|All files (*.*)|*.*"; // Фильтры файлов
+
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                // Получаем путь к выбранному файлу
+                string filename = dlg.FileName;
+                AddTrack(filename);
             }
         }
 
-        private void ExecuteTogglePlayCommand()
+        private void AddTrack(string path)
         {
-            IsPlaying = !IsPlaying;
-            OnPropertyChanged("TriggerAnimation");
+            // Извлечение имени файла для использования в качестве ключа или любой другой уникальной строки
+            var key = Path.GetFileNameWithoutExtension(path);
+
+            if (!_tracksDictionary.ContainsKey(key))
+            {
+                Track newTrack = new Track { Author = "Unknown", TrackName = key, Path = path };
+                _tracksDictionary.Add(key, newTrack);
+                OnPropertyChanged(nameof(TracksDictionary));
+                SaveTracks();  // Сохраняем треки после каждого добавления
+            }
+            else
+            {
+                Debug.WriteLine("Track already exists.");
+            }
+        }
+        public void SaveTracks()
+        {
+            try
+            {
+                string json = JsonSerializer.Serialize(_tracksDictionary);
+                File.WriteAllText(TracksFilePath, json);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error saving tracks: " + ex.Message);
+                // Здесь можете добавить более подробную обработку ошибок
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -258,8 +127,15 @@ namespace rm
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-    }
 
+        ~ViewModel()
+        {
+            SaveTracks();
+        }
+    }
+    #endregion
+
+    #region etc
     public class RelayCommand : ICommand
     {
         private readonly Action _execute;
@@ -286,59 +162,25 @@ namespace rm
         {
             _execute();
         }
+        #endregion
     }
-
+    #endregion
+    #region WorkWithWPF
     public partial class Window1 : Window
-    {
-
-        //private void HackInit()
-        //{
-        //    string Host = System.Net.Dns.GetHostName();
-        //    string IP = System.Net.Dns.GetHostByName(Host).AddressList[0].ToString();
-        //    string PCName = Environment.MachineName;
-        //    string ConnString = "server=188.187.190.68;user=ftp;password=qweqwe123;database=recmusic;";
-        //    string mbInfo = String.Empty;
-        //    ManagementScope scope = new ManagementScope("\\\\" + Environment.MachineName + "\\root\\cimv2");
-        //    scope.Connect();
-        //    ManagementObject wmiClass = new ManagementObject(scope, new ManagementPath("Win32_BaseBoard.Tag=\"Base Board\""), new ObjectGetOptions());
-
-        //    foreach (PropertyData propData in wmiClass.Properties)
-        //    {
-        //        if (propData.Name == "SerialNumber")
-        //        {
-        //            mbInfo = Convert.ToString(propData.Value);
-        //            break;
-        //        }
-        //    }
-        //    using (var connection = new MySqlConnection(ConnString))
-        //    {
-        //        try
-        //        {
-        //            connection.Open();
-        //            string sql = "INSERT INTO recmusic (PCName, IP, HWID) VALUES (@pcName, @ip, @hwid)";
-        //            using (var cmd = new MySqlCommand(sql, connection))
-        //            {
-        //                cmd.Parameters.AddWithValue("@pcName", PCName);
-        //                cmd.Parameters.AddWithValue("@ip", IP);
-        //                cmd.Parameters.AddWithValue("@hwid", mbInfo);
-        //                cmd.ExecuteNonQuery();
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            MessageBox.Show("Error: " + ex);
-        //        }
-        //    }
-        //    //Process.Start("chrome.exe", "https://vk.com/music/album/-2000317188_20317188_18ad97b4c0c6cb394c?act=album");
-        //}
-
-
+    {      
         public Window1()
         {
             InitializeComponent();
-            //HackInit();
+            Closing += OnWindowClosing;
+            this.DataContext = new ViewModel();            
         }
-
+        private void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            if (DataContext is ViewModel vm)
+            {
+                vm.SaveTracks();
+            }
+        }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             UpdateClip();
@@ -376,8 +218,7 @@ namespace rm
         // Работа с верхней панелью
 
         private void CloseWindow(object sender, RoutedEventArgs e)
-        {
-            this.Close();
+        {            this.Close();
         }
 
         private void GoFullScreenButton(object sender, RoutedEventArgs e)
@@ -419,21 +260,30 @@ namespace rm
         }
 
         //  Бар меню
-        
+
 
         private void PlayClick(object sender, RoutedEventArgs e)
         {
             if (DataContext is ViewModel vm)
             {
                 vm.IsPlaying = !vm.IsPlaying;
-                
             }
         }
     }
 }
-
+#endregion
+    #region Classes
 public class Item
 {
     public int Id { get; set; }
     public string Name { get; set; }
 }
+
+public class Track
+{
+    public string Author { get; set; }
+    public string TrackName { get; set; }
+    public string Path { get; set; }
+    // Здесь можете добавить свои свойства, например, Image обложки
+}
+#endregion
